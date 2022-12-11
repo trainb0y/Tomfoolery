f36f368e1e586c9d94e5e99010a759e61bf2d747
trainb0y <trainb0ydev@gmail.com>
2022-12-10 19:37:27 -0700

    Basic fabric mod

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	id("fabric-loom")
+	`maven-publish`
+	java
+	kotlin("jvm") version "1.7.21"
+}
+
+group = property("maven_group")!!
+version = property("mod_version")!!
+
+repositories {
+	// Add repositories to retrieve artifacts from in here.
+	// You should only use this when depending on other mods because
+	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
+	// for more information about repositories.
+	maven(uri("https://maven.gegy.dev"))
+	mavenCentral()
+}
+
+dependencies {
+	minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+
+	modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+	modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
+}
+
+tasks {
+	processResources {
+		inputs.property("version", project.version)
+		filesMatching("fabric.mod.json") {
+			expand(mutableMapOf("version" to project.version))
+		}
+	}
+
+	jar {
+		from("LICENSE")
+	}
+
+	compileKotlin {
+		kotlinOptions.jvmTarget = "17"
+	}
+}
+
+java {
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("fabric-loom") version "1.0-SNAPSHOT"
+    }
+}
